122d9412fb133be3c3eb9875ddf6976517f38471
haichungcn <haichungcn@gmail.com>
2024-04-15 10:44:53 +0700

    move stage children to stage content component to keep the states separated from others

---
+++ src/components/stage-content.tsx
@@ -0,0 +1,65 @@
+import { useMemo, useRef, useState } from "react";
+import { Layer, Circle } from "react-konva";
+import { MOCK_FLOOR_URL, STAGE_WIDTH, STAGE_HEIGHT } from "../constants";
+import { Dimension, ImageStatus, StageChildrenProps } from "../type";
+import FloorPlanImage from "./floor-plan-image";
+import Pin from "./pin";
+
+const getPinArrays = (floorplanDimension: Dimension | null) =>
+  !!floorplanDimension
+    ? Array.from({ length: 20 }, () => ({
+        x: Math.random() * floorplanDimension.width,
+        y: Math.random() * floorplanDimension.height,
+      }))
+    : [];
+
+const StageContent = ({
+  startingPosition,
+  setStartingPosition,
+  scale,
+}: StageChildrenProps) => {
+  const floorplanDimension = useRef<Dimension | null>(null);
+  const [floorplanStatus, setFloorplanStatus] = useState<ImageStatus>();
+  const pinArrays = useMemo(
+    () => getPinArrays(floorplanDimension.current),
+    [floorplanStatus],
+  );
+  return (
+    <>
+      <Layer>
+        <FloorPlanImage
+          url={MOCK_FLOOR_URL}
+          stageDimension={{ width: STAGE_WIDTH, height: STAGE_HEIGHT }}
+          onImageLoad={(initialState) => {
+            setStartingPosition(initialState.startingPosition);
+            floorplanDimension.current = initialState.startingDimension;
+            setFloorplanStatus(initialState.status);
+          }}
+        />
+      </Layer>
+      <Layer>
+        {floorplanStatus === "loaded" &&
+          pinArrays.map(({ x, y }) => (
+            <>
+              <Pin
+                x={startingPosition.x + x}
+                y={startingPosition.y + y}
+                startingPosition={startingPosition}
+                stageScale={scale}
+              />
+              <Circle
+                x={startingPosition.x + x}
+                y={startingPosition.y + y}
+                width={8}
+                height={8}
+                fill="red"
+                scale={{ x: 1 / scale.x, y: 1 / scale.y }}
+              />
+            </>
+          ))}
+      </Layer>
+    </>
+  );
+};
+
+export default StageContent;
